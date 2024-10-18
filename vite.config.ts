49eaf158787f37329657da73819dfc586cbdd943
AndrewBubnov <jackblack.nt@gmail.com>
2024-10-19 01:52:24 +0500

    feat: specify dev serv port

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  server: { port: 3000 },
   resolve: {
     alias: {
       assets: path.resolve('src/shared/assets'),
