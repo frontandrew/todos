163384f33633acff24805cfc9d0e7c8af471d9d3
AndrewBubnov <jackblack.nt@gmail.com>
2024-10-20 09:49:52 +0500

    feat: main page layout

--- src/pages/main/view.tsx
+++ src/pages/main/view.tsx
@@ -1,10 +1,18 @@
-import { Logo } from 'uikit'
 import { FC } from 'react'
+
+import { BurgerIngredients } from 'features/burger-ingredients'
+
+import style from './style.module.css'
 
 export const Main: FC = () => {
   return (
-    <>
-      <Logo />
-    </>
+    <div className={style.container}>
+      <section className={style.content}>
+        <BurgerIngredients />
+      </section>
+      <section className={style.content}>
+
+      </section>
+    </div>
   )
 }
