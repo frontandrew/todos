7d45f0e5187cd16f9f397083257ad102f21c4541
AndrewBubnov <jackblack.nt@gmail.com>
2024-10-28 11:20:06 +0500

    feat: get ingredients data by app mount

--- src/app/view.tsx
+++ src/app/view.tsx
@@ -1,11 +1,41 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
-import { AppHeader } from 'features/app-header'
 import { Main } from 'pages'
+import { AppHeader } from 'features/app-header'
+import { Ingredient } from 'entities/ingredient'
+
+import { formatIngredientsResponse, IngredientsResponseData } from 'utils'
+import { INGREDIENTS_HOST } from 'consts'
 
 import style from './style.module.css'
 
 export const App: FC = () => {
+  const [ingredients, setIngredients] = useState<Ingredient[]>()
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
+
+  useEffect(() => {
+    const result = async () => await fetch(INGREDIENTS_HOST)
+      .then((response) => {
+        const { status, statusText } = response
+
+        if ((status >= 200) && (status <= 399)) return response.json()
+        throw new Error(
+          `request failed with status: ${status} by ${statusText || 'unknown reason'}.`
+        )
+      })
+      .then(({ data }: { data: IngredientsResponseData }) => {
+        setIngredients(formatIngredientsResponse(data))
+      })
+      .catch((reason) => {
+        setHasError(Boolean(reason))
+        console.error(reason)
+      })
+      .finally(() => setIsLoading(false))
+
+    result()
+  }, [])
+
   return (
     <div className={style.container}>
       <header className={style.header}>
