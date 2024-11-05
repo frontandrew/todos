93ca0073f5576496e6f84e9defa408a15f03cce5
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-05 20:29:19 +0500

    feat: connect app state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
+
+import { setupStore } from 'store'
 
 import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={setupStore()}>
+      <App />
+    </Provider>
   </StrictMode>
 )
