18febed6c1a544f465702e40a7f28635c150aacc
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-06 05:09:56 +0500

    fix: optimize ingredients separate func

--- src/features/burger-constructor/view.tsx
+++ src/features/burger-constructor/view.tsx
@@ -9,17 +9,14 @@ import { Order, OrderDetails } from 'entities/order'
 
 import style from './style.module.css'
 
-/** TODO: декомпозировать на более мелкие и радтелить стэйт */
+/** TODO: возможно, декомпозировать на более мелкие и радтелить стэйт */
 export const BurgerConstructor: FC<{ order: Order }> = ({ order }) => {
   const { isModalOpen, closeModal, openModal } = useModal()
 
-  const bun = useMemo(() => order.ingredients.find(
-    ({ type }) => type === IngredientType[0]
-  ), [order.ingredients])
-
-  const otherIngredients = useMemo(() => order.ingredients.filter(
-    ({ type }) => type !== IngredientType[0]
-  ), [order.ingredients])
+  const [bun, otherIngredients] = useMemo(() => [
+    order.ingredients.find(({ type }) => type === IngredientType[0]),
+    order.ingredients.filter(({ type }) => type !== IngredientType[0]),
+  ], [order.ingredients])
 
   const handleOrderSubmit = useCallback((e: SyntheticEvent) => {
     e.stopPropagation()
@@ -27,33 +24,38 @@ export const BurgerConstructor: FC<{ order: Order }> = ({ order }) => {
   }, [openModal])
 
   return (
-    <article className={style.container + ' pt-25 pb-10'}>
+    <>
+      {order.ingredients.length > 0 &&
+        <article className={style.container + ' pt-25 pb-10'}>
 
-      <div className={style.content}>
-        {bun && <IngredientItem ingredient={bun} isLocked={true} type='top' />}
-        <ul className={style.draggable}>
-          {otherIngredients.map(item => <IngredientItem ingredient={item} key={item.id} />)}
-        </ul>
-        {bun && <IngredientItem ingredient={bun} isLocked={true} type='bottom' />}
-      </div>
+          <div className={style.content}>
+            {bun && <IngredientItem ingredient={bun} isLocked={true} type='top' />}
+            <ul className={style.draggable}>
+              {otherIngredients.map(item => <IngredientItem ingredient={item} key={item.id} />)}
+            </ul>
+            {bun && <IngredientItem ingredient={bun} isLocked={true} type='bottom' />}
+          </div>
 
-      <div className={style.footer}>
-        <div className={style.total}>
-          <span className='text text_type_digits-medium'>{order.total}</span>
-          <CurrencyIcon type='primary' />
-        </div>
-        <Button
-          type='primary'
-          htmlType='submit'
-          disabled={order.ingredients.length < 3}
-          onClick={handleOrderSubmit}
-        >
-          Оформить заказ
-        </Button>
-      </div>
+          <div className={style.footer}>
+            <div className={style.total}>
+              <span className='text text_type_digits-medium'>{order.total}</span>
+              <CurrencyIcon type='primary' />
+            </div>
+            <Button
+              type='primary'
+              htmlType='submit'
+              disabled={order.ingredients.length < 3}
+              onClick={handleOrderSubmit}
+            >
+              Оформить заказ
+            </Button>
+          </div>
+        </article>
+      }
+
       <Modal close={closeModal} isVisible={isModalOpen}>
         <OrderDetails {...order} />
       </Modal>
-    </article>
+    </>
   )
 }
