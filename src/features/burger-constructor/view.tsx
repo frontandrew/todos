f2ecf0df9c73a1600f21ea13bd451a8737313ef5
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-06 23:16:36 +0500

    feat: empty order state view

--- src/features/burger-constructor/view.tsx
+++ src/features/burger-constructor/view.tsx
@@ -7,6 +7,7 @@ import { useAppSelector, useModal } from 'hooks'
 import { IngredientItem, IngredientType } from 'entities/ingredient'
 import { OrderDetails } from 'entities/order'
 
+import { EmptyBurgerConstructor } from './componets'
 import style from './style.module.css'
 
 /** TODO: возможно, декомпозировать на более мелкие и радтелить стэйт */
@@ -27,33 +28,36 @@ export const BurgerConstructor: FC = () => {
 
   return (
     <>
-      {order.ingredients.length > 0 &&
-        <article className={style.container + ' pt-25 pb-10'}>
-
-          <div className={style.content}>
-            {bun && <IngredientItem ingredient={bun} isLocked={true} type='top' />}
-            <ul className={style.draggable}>
-              {otherIngredients.map(item => <IngredientItem ingredient={item} key={item.id} />)}
-            </ul>
-            {bun && <IngredientItem ingredient={bun} isLocked={true} type='bottom' />}
-          </div>
-
-          <div className={style.footer}>
-            <div className={style.total}>
-              <span className='text text_type_digits-medium'>{order.total}</span>
-              <CurrencyIcon type='primary' />
+      <article className={style.container + ' pt-25 pb-10'}>
+        {order.id ?
+          <>
+            <div className={style.content}>
+              {bun && <IngredientItem ingredient={bun} isLocked={true} type='top' />}
+              <ul className={style.draggable}>
+                {otherIngredients.map(item => <IngredientItem ingredient={item} key={item.id} />)}
+              </ul>
+              {bun && <IngredientItem ingredient={bun} isLocked={true} type='bottom' />}
             </div>
-            <Button
-              type='primary'
-              htmlType='submit'
-              disabled={order.ingredients.length < 3}
-              onClick={handleOrderSubmit}
-            >
-              Оформить заказ
-            </Button>
-          </div>
-        </article>
-      }
+
+            <div className={style.footer}>
+              <div className={style.total}>
+                <span className='text text_type_digits-medium'>{order.total}</span>
+                <CurrencyIcon type='primary' />
+              </div>
+              <Button
+                type='primary'
+                htmlType='submit'
+                disabled={order.ingredients.length < 3}
+                onClick={handleOrderSubmit}
+              >
+                Оформить заказ
+              </Button>
+            </div>
+          </>
+          :
+          <EmptyBurgerConstructor />
+        }
+      </article>
 
       <Modal close={closeModal} isVisible={isModalOpen}>
         <OrderDetails {...order} />
