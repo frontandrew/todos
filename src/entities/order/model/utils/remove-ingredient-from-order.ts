62d5f87a1a1b364e9646f00db510b67e6f0abdb2
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-07 23:44:31 +0500

    fix: remove from order item index

--- src/entities/order/model/utils/remove-ingredient-from-order.ts
+++ src/entities/order/model/utils/remove-ingredient-from-order.ts
@@ -1,7 +1,7 @@
 import { RenoveIngredientFromOrder } from './type'
 
 export const removeIngredientFromOrder: RenoveIngredientFromOrder = (items, index) => {
-  const removedIndex = items.findIndex(({ orderIngredientIndex: id }) => index === id )
+  const removedIndex = items.findIndex(({ orderIngredientIndex: id }) => index === id ) - 1
 
   if (removedIndex >= 0) {
     return [...items.splice(removedIndex, 1)]
