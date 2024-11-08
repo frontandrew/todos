67988d62b006ad0bcdac2d38b641ae850f32952c
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-08 05:50:07 +0500

    feat: create change position func for order model use

--- src/entities/order/model/utils/index.ts
+++ src/entities/order/model/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './add-ingredient-into-order'
 export * from './calc-order-total'
+export * from './change-ingredient-position'
 export * from './remove-ingredient-from-order'
--- src/entities/order/model/utils/type.ts
+++ src/entities/order/model/utils/type.ts
@@ -5,3 +5,4 @@ import { OrderIngredientItem } from 'entities/order'
 export type AddIngredientIntoOrder = (items: OrderIngredientItem[], item: Ingredient) => OrderIngredientItem[]
 export type RenoveIngredientFromOrder = (items: OrderIngredientItem[], index: string) => OrderIngredientItem[]
 export type CalcOrderTotal = (items: { [x: string]: any, price: number, type: IngredientTypes }[]) => number
+export type ChangeIngredientPosition = (items: OrderIngredientItem[], curr: string, targ: string) => OrderIngredientItem[]
