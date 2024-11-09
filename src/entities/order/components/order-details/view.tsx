9be3410e1cefa8cf76dae3f1f3390a581b023219
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-10 02:20:44 +0500

    chore: order title format

--- src/entities/order/components/order-details/view.tsx
+++ src/entities/order/components/order-details/view.tsx
@@ -7,7 +7,9 @@ import style from './style.module.css'
 
 export const OrderDetails: FC<Order> = ({ id }) => (
   <article className={style.container + ' pb-20'}>
-    <h3 className={style.title + ' text text_type_digits-large pb-8'}>{id}</h3>
+    <h3 className={style.title + ' text text_type_digits-large pb-8'}>
+      {id && id.toLocaleUpperCase()}
+    </h3>
     <h4 className={'text text_type_main-medium'}>идентификатор заказа</h4>
     <img className={'p-15'} src={orderDoneIcon} alt={'Ваш заказ начали готовить'} />
     <p className={'text text_type_main-small pb-2'}>Ваш заказ начали готовить</p>
