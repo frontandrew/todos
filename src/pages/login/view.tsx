c067c6ce104490c7869575f65336db348bacf057
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-21 12:00:21 +0500

    feat: create login page

---
+++ src/pages/login/view.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import { Input, Button } from 'uikit'
+
+import style from './style.module.css'
+
+export const LoginPage: FC = () => {
+  return (
+    <div className={style.container}>
+      <form className={style.form}>
+        <h1 className={'text text_type_main-medium'}>Вход</h1>
+        <Input
+          onChange={() => {
+          }}
+          type={'email'}
+          placeholder={'E-mail'}
+          value={''}
+        />
+        <Input
+          onChange={() => {
+          }}
+          type={'password'}
+          placeholder={'Password'}
+          value={''}
+        />
+        <Button htmlType="submit">Войти</Button>
+      </form>
+      <nav>
+        <ul className={style.navlist}>
+          <li className={style.navitem}>
+            <span className={'text text_type_main-small'}>
+              Вы — новый пользователь?
+            </span>
+            <span className={'text text_type_main-small text_color_accent'}>
+              <NavLink to={'/register'}>
+                Зарегистрироваться
+              </NavLink>
+            </span>
+          </li>
+          <li className={style.navitem}>
+            <span className={'text text_type_main-small'}>
+              Забыли пароль?
+            </span>
+            <span className={'text text_type_main-small text_color_accent'}>
+              <NavLink to={'/forgot-password'}>
+                Восстановить пароль
+              </NavLink>
+            </span>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
