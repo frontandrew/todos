88f49f3c5671b6f100295262bcf8aa79abaa8c0c
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-24 22:04:48 +0500

    feat: create useForm hook

---
+++ src/shared/hooks/use-form/hook.ts
@@ -0,0 +1,33 @@
+import { FormEvent, useCallback, useEffect, useRef, useState } from 'react'
+
+export const useForm = <T>({ submitHandler, formInitValues }: { submitHandler:
+(args: T) => void, formInitValues: T
+}) => {
+  const formRef = useRef<HTMLFormElement | null>(null)
+  const [formValues, setFormValues] = useState<{[K in keyof T]: T[K]}>(formInitValues)
+
+  useEffect(() => {
+    const firstInput = formRef.current?.elements[0]
+    if (firstInput) (firstInput as HTMLInputElement).focus()
+  }, [])
+
+  const formChange = (event: FormEvent) => {
+    const { value, name } = event.target as HTMLInputElement
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const formSubmit = useCallback((event: FormEvent) => {
+    event.preventDefault()
+    const isValid = Object
+      .values(formValues)
+      .every(value => Boolean(value))
+    if (submitHandler && isValid) submitHandler({ ...formValues })
+  }, [formValues, submitHandler])
+
+  const formReset = useCallback((event: FormEvent) => {
+    event.preventDefault()
+    setFormValues(formInitValues)
+  }, [formInitValues])
+
+  return { formRef, formValues, formChange, formSubmit, formReset }
+}
