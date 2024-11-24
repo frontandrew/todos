31fea938b7889e9234ad194f74fb2b0f883fecb8
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-24 23:50:32 +0500

    chore: use forgot pass form state hook

--- src/pages/forgot-pass/view.tsx
+++ src/pages/forgot-pass/view.tsx
@@ -2,19 +2,30 @@ import { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import { Button, EmailInput } from 'uikit'
+import { useForm } from 'hooks'
 
 import style from './style.module.css'
 
 export const ForgotPassPage: FC = () => {
+  const { formRef, formValues, formChange, formSubmit } = useForm({
+    submitHandler: (args) => console.log(args),
+    formInitValues: { email: '' },
+  })
+
   return (
     <div className={style.container}>
-      <form className={style.form}>
+      <form
+        className={style.form}
+        ref={formRef}
+        onChange={formChange}
+        onSubmit={formSubmit}
+      >
         <h1 className={'text text_type_main-medium'}>Восстановление пароля</h1>
         <EmailInput
-          onChange={() => {
-          }}
+          onChange={() => {}}
           placeholder={'Укажите e-mail'}
-          value={''}
+          value={formValues.email || ''}
+          name={'email'}
         />
         <Button htmlType="submit">Восстановить</Button>
       </form>
