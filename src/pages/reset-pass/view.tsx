2f6ee3471475a15f08d9779622e934f7f35a984b
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-25 11:33:44 +0500

    chore: use reset pass form state hook

--- src/pages/reset-pass/view.tsx
+++ src/pages/reset-pass/view.tsx
@@ -2,25 +2,38 @@ import { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import { Input, Button, PasswordInput } from 'uikit'
+import { useForm } from 'hooks'
 
 import style from './style.module.css'
 
 export const ResetPassPage: FC = () => {
+  const { formRef, formValues, formChange, formSubmit } = useForm({
+    submitHandler: (args) => console.log(args),
+    formInitValues: { password: '', code: '' },
+  })
+
   return (
     <div className={style.container}>
-      <form className={style.form}>
+      <form
+        className={style.form}
+        onSubmit={formSubmit}
+        onChange={formChange}
+        ref={formRef}
+      >
         <h1 className={'text text_type_main-medium'}>Восстановление пароля</h1>
         <PasswordInput
           onChange={() => {
           }}
           placeholder={'Введите новый пароль'}
-          value={''}
+          value={formValues.password || ''}
+          name={'password'}
         />
         <Input
           onChange={() => {
           }}
           placeholder={'Введите код из письма'}
-          value={''}
+          value={formValues.code || ''}
+          name={'code'}
         />
         <Button htmlType="submit">Сохранить</Button>
       </form>
