ff05e7d8ef1d8b645ed55503211aec886f7ca5c4
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-25 23:43:48 +0500

    feat: use user and auth models

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -5,12 +5,16 @@ import { ingredientsSlice } from 'features/burger-ingredients'
 import { currentIngredientSlice } from 'entities/ingredient'
 import { currentOrderSlice } from 'entities/order'
 import { appLoaderSlice } from 'features/app-loader'
+import { userSlice } from 'entities/user'
+import { authSlice } from 'features/authentification'
 
 export const rootReducer = combineReducers({
   [currentOrderSlice.reducerPath]: currentOrderSlice.reducer,
   [currentIngredientSlice.reducerPath]: currentIngredientSlice.reducer,
   [ingredientsSlice.reducerPath]: ingredientsSlice.reducer,
   [appLoaderSlice.reducerPath]: appLoaderSlice.reducer,
+  [userSlice.reducerPath]: userSlice.reducer,
+  [authSlice.reducerPath]: authSlice.reducer,
 
   [apiSlice.reducerPath]: apiSlice.reducer,
 })
