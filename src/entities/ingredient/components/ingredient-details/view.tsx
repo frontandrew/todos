88ede92eeea8384c586444d24235ef4fb787e5f2
AndrewBubnov <jackblack.nt@gmail.com>
2024-11-30 17:22:58 +0500

    chore: ingredient details now get data from current history location

--- src/entities/ingredient/components/ingredient-details/view.tsx
+++ src/entities/ingredient/components/ingredient-details/view.tsx
@@ -1,22 +1,36 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import { useAppSelector } from 'hooks'
 
+import { Ingredient } from '../../type'
 import { IngredientNutrients } from './components'
 import style from './style.module.css'
 
 export const IngredientDetails: FC = () => {
-  const { ingredient: data } = useAppSelector(state => state.currentIngredient)
+  const [data, setData] = useState<Ingredient | undefined>()
+  const location = useLocation()
+  const ingredients = useAppSelector(state => state.ingredients)
+
+  useEffect(() => {
+    if (location.pathname.startsWith('/ingredients/')) {
+      const ingredientId = location.pathname.split('/').reverse()[0]
+      const data = ingredients.find(({ id }) => id === ingredientId)
+
+      setData(data)
+    }
+  }, [location, ingredients])
+
 
   return (data &&
     <article className={style.container}>
-      <img className={style.image} src={data.imageLarge} alt={data.name} />
+      <img className={style.image} src={data.imageLarge} alt={data.name}/>
       <h4 className={'text text_type_main-medium pt-4 pb-8'}>{data.name}</h4>
       <IngredientNutrients {...{
         carbohydrates: data.carbohydrates,
         calories: data.calories,
         proteins: data.proteins,
         fat: data.fat,
-      }} />
+      }}/>
     </article>
   )
 }
