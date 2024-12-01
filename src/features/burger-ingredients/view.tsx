b9ebc8c9c020eb3e2817bd673086810d4280bec8
AndrewBubnov <jackblack.nt@gmail.com>
2024-12-02 00:11:55 +0500

    chore: tabs on scroll select refactoring

--- src/features/burger-ingredients/view.tsx
+++ src/features/burger-ingredients/view.tsx
@@ -1,14 +1,43 @@
-import { FC, useCallback, useRef } from 'react'
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { InView } from 'react-intersection-observer'
 
 import { Tabs } from 'components'
-import { IngredientType } from 'entities/ingredient'
+import { useAppSelector } from 'hooks'
 
-import { IngredientsList } from './components'
+import { Ingredient, IngredientCard } from 'entities/ingredient'
+
 import { IngredientsGroupNames } from './consts'
 import style from './style.module.css'
 
 export const BurgerIngredients: FC = () => {
   const listRef = useRef<HTMLUListElement>(null)
+  const [currentTab, setTab] = useState(Object.keys(IngredientsGroupNames)[0])
+
+  const ingredients = useAppSelector(state => state.ingredients)
+  const ingredientsMap = useMemo<Record<string, Ingredient[]>>(() => Object
+    .keys(IngredientsGroupNames)
+    .reduce(
+      (acc, key) => (
+        { ...acc, [key]: ingredients.filter(({ type }) => type === key) }
+      ), {},
+    ), [ingredients])
+
+  const thresholds = useMemo(() => {
+    const thresholdsData = Object
+      .entries(ingredientsMap)
+      .reduce((acc, [type, ingrs]) => {
+        return ({ ...acc, total: acc.total + ingrs.length, [type]: ingrs.length })
+      }, { total: 0 })
+    const { total, ...rest } = thresholdsData
+
+    return Object.entries(rest).reduce((acc, [type, length]) => {
+      const empiricalCoefficient = 0.08
+      return { ...acc, [type]: (Math.ceil((length as number) * 100 / total) / 100) - empiricalCoefficient }
+    }, {})
+  }, [ingredientsMap])
+
+  console.log(thresholds)
+
 
   const handleTabClick = useCallback((tabId: string) => {
     const targetEl = listRef.current?.querySelector(`#${tabId}`)
@@ -22,10 +51,35 @@ export const BurgerIngredients: FC = () => {
         <Tabs
           onClick={handleTabClick}
           tabsNameValueMap={IngredientsGroupNames}
-          initialTab={IngredientType.BUN}
+          currentTab={currentTab}
         />
       </div>
-      <IngredientsList ref={listRef} />
+      <ul className={style.groups} ref={listRef}>
+        {Object.entries(ingredientsMap).map(([key, ingrs]) =>
+          ingrs.length > 0 &&
+          <InView
+            root={document.querySelector(style.groups)}
+            onChange={(state, e) => {
+              if (state) {
+                setTab(key)
+                console.log(key, e.intersectionRatio)
+              }
+            }}
+            threshold={(thresholds as Record<string, number>)[key]}
+            className={style.group}
+            key={key}
+            as={'li'}
+            id={key}
+          >
+            <h3 className={'text text_type_main-medium'}>{IngredientsGroupNames[ingrs[0].type]}</h3>
+            <ul className={style.items + ' pr-4 pl-4'}>
+              {ingrs.map((ingr) =>
+                <IngredientCard ingredient={ingr} key={ingr.id}/>,
+              )}
+            </ul>
+          </InView>,
+        )}
+      </ul>
     </article>
   )
 }
