05ea08456fdab7b79bc1388632832c0105003e90
AndrewBubnov <jackblack.nt@gmail.com>
2024-12-01 16:46:30 +0500

    feat: logo as home to home navigation item

--- src/features/app-header/view.tsx
+++ src/features/app-header/view.tsx
@@ -1,23 +1,25 @@
-import { Logo } from 'uikit'
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
+
+import { Logo } from 'uikit'
 import { NavItem } from 'components'
 
 import { appHeaderItems } from './items'
 import style from './style.module.css'
 
-export const AppHeader: FC = () => {
-  return (
-    <header className={style.header}>
-      <nav className={style.container}>
-        <Logo className={style.logo}/>
-        <ul className={style.list}>
-          {Object.entries(appHeaderItems).map(([key, item]) =>
-            <li className={item.to ? style.item : style.item_spacer} key={key}>
-              {item.to && <NavItem {...item} size={'small'}/>}
-            </li>
-          )}
-        </ul>
-      </nav>
-    </header>
-  )
-}
+export const AppHeader: FC = () => (
+  <header className={style.header}>
+    <nav className={style.container}>
+      <Link to={'/'} className={style.logo}>
+        <Logo/>
+      </Link>
+      <ul className={style.list}>
+        {Object.entries(appHeaderItems).map(([key, item]) =>
+          <li className={item.to ? style.item : style.item_spacer} key={key}>
+            {item.to && <NavItem {...item} size={'small'}/>}
+          </li>,
+        )}
+      </ul>
+    </nav>
+  </header>
+)
