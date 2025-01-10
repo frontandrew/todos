51ad253a63e6308edf48f12144ceddb5f66a266e
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-11 01:58:05 +0500

    fix: sorting orders list for different affiliations

--- src/features/orders/components/orders-list/view.tsx
+++ src/features/orders/components/orders-list/view.tsx
@@ -10,22 +10,25 @@ import style from './style.module.css'
 
 export const OrdersList: FC<{ affiliation: OrdersAffiliation }> = ({ affiliation }) => {
   const dispatch = useAppDispatch()
-  const { orders } = useAppSelector(ordersSlice.selectors.state)
+  let { orders: sortedOrders } = useAppSelector(ordersSlice.selectors.state)
   const ingrs = useAppSelector(ingredientsSlice.selectors.getState)
     .reduce((acc, ingr) => acc.set(ingr.id, ingr), new Map<string, Ingredient>())
 
+  if (affiliation === 'user' && sortedOrders.length > 0) {
+    sortedOrders = [...sortedOrders].reverse()
+  }
 
   useEffect(() => {
     dispatch(ordersSlice.actions.startWatchOrders(affiliation))
     return () => {
       dispatch(ordersSlice.actions.stopWatchOrders())
+      dispatch(ordersSlice.actions.resetOrders())
     }
   }, [])
 
   return (
     <ul className={style.container}>
-      {orders.length <= 0 ? null : [...orders]
-        .reverse()
+      {sortedOrders.length <= 0 ? null : sortedOrders
         .map(({ ingredients, ...rest }) => {
           const orderIngredients = ingredients
             .reduce((acc, ingrId) => {
