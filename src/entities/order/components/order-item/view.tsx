8b5f74ccc9907a8096126bcb33d8b8ea20ed2e92
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-10 05:49:44 +0500

    feat: order item layouting

--- src/entities/order/components/order-item/view.tsx
+++ src/entities/order/components/order-item/view.tsx
@@ -8,45 +8,48 @@ import { OrderStatusesMap } from 'entities/order'
 import { IngredientIcon } from 'entities/ingredient'
 
 export const OrderItem: FC<OrderItemProps> = memo(({
-  name,
-  number,
-  status: stat,
-  createdAt,
-  updatedAt,
-  ingredients,
-}) => {
-  const date = new Date(updatedAt ?? createdAt)
-  const total = ingredients.reduce((acc, { price }) => acc + price, 0)
-  const status = OrderStatusesMap[stat]
-  const ifStatIsDoneClass = stat === 'done' ? ' text_color_success' : ''
+    name,
+    number,
+    status: stat,
+    createdAt,
+    updatedAt,
+    ingredients,
+  }) => {
+    const date = new Date(updatedAt ?? createdAt)
+    const total = ingredients.reduce((acc, { price }) => acc + price, 0)
+    const status = OrderStatusesMap[stat]
+    const ifStatIsDoneClass = stat === 'done' ? ' text_color_success' : ''
 
 
-  return (
-    <article className={style.container}>
-      <div className={style.header}>
-        <p className={'text text_type_digits-default'}>{`#${number}`}</p>
-        <FormattedDate
-          className={'text text_type_main-default text_color_inactive'}
-          date={date}
-        />
-      </div>
-      <div className={style.title}>
-        <h3 className={'text text_type_main-medium'}>{name}</h3>
-        <span className={'text text_type_main-default' + ifStatIsDoneClass}>{status}</span>
-      </div>
-      <div className={style.details}>
-        <ul className={style.components}>
-          {ingredients.map(
-            (ingredient, index) => <IngredientIcon
-              key={`${ingredient.id}-${index}`}
-              {...ingredient}
-            />
-          )}
-        </ul>
-        <PriceWithCurrency value={total}/>
-      </div>
-    </article>
-)
-}, (prevProps, nextProps) =>
-prevProps.status === nextProps.status,
+    return (
+      <article className={style.container}>
+        <div className={style.header}>
+          <p className={'text text_type_digits-default'}>{`#${number}`}</p>
+          <FormattedDate
+            className={'text text_type_main-default text_color_inactive'}
+            date={date}
+          />
+        </div>
+        <div className={style.title}>
+          <h3 className={'text text_type_main-medium'}>{name}</h3>
+          <span className={'text text_type_main-default' + ifStatIsDoneClass}>{status}</span>
+        </div>
+        <div className={style.details}>
+          <ul className={style.components}>
+            {ingredients.map(
+              (ingredient, index) =>
+                <li
+                  className={style.component}
+                  key={`${ingredient.id}-${index}`}
+                >
+                  <IngredientIcon {...ingredient}/>
+                </li>
+            )}
+          </ul>
+          <PriceWithCurrency value={total}/>
+        </div>
+      </article>
+    )
+  }, (prevProps, nextProps) =>
+    prevProps.status === nextProps.status,
 )
