ac32066a2a964293b7407d071c258871ce4bc7a7
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-11 09:51:41 +0500

    feat: order details feature

--- src/entities/order/components/order-status-colored/view.tsx
+++ src/entities/order/components/order-status-colored/view.tsx
@@ -2,11 +2,11 @@ import { FC } from 'react'
 import { OrderStatus } from 'entities/order'
 import { OrderStatusesMap } from '../../constants.ts'
 
-export const OrderStatusColored: FC<{ text: OrderStatus, classNames?: string }> = ({ text, classNames }) => {
+export const OrderStatusColored: FC<{ text: OrderStatus, className?: string }> = ({ text, className }) => {
 
   const status = OrderStatusesMap[text]
   const ifStatIsDoneClass = text === 'done' ? ' text_color_success' : ''
-  const classes = `text text_type_main-default ${ifStatIsDoneClass} ${classNames ?? ''}`
+  const classes = `text text_type_main-default ${ifStatIsDoneClass} ${className ?? ''}`
 
 
   return <span className={classes}>{status}</span>
---
+++ src/features/order-details/view.tsx
@@ -0,0 +1,40 @@
+import { FC, memo } from 'react'
+import { FormattedDate } from 'uikit'
+import { PriceWithCurrency } from 'components'
+import { useAppSelector } from 'hooks'
+
+import { Order, OrderStatusColored } from 'entities/order'
+import { ingredientsSlice } from 'features/burger-ingredients' //TODO: sin
+
+import style from './style.module.css'
+
+export const OrderDetails: FC<Order> = memo(({
+  ingredients: ingrIds,
+  createdAt,
+  updatedAt,
+  number,
+  name,
+  status,
+}) => {
+  const ingredients = useAppSelector(ingredientsSlice.selectors.getState)
+    .filter(({ id }) => ingrIds.includes(id))
+  const total = ingredients.reduce((acc, { price }) => acc + price, 0)
+  const date = new Date(updatedAt ?? createdAt)
+
+  return (
+    <article className={style.container}>
+      <p className={'text text_type_digits-default pb-10'}>{`#${number}`}</p>
+      <h3 className={'text text_type_main-medium pb-3'}>{name}</h3>
+      <OrderStatusColored className={'pb-15'} text={status}/>
+      <p className={'text text_type_main-medium pb-6'}>Состав:</p>
+      <ul className={style.list}>
+        {/*  <IngredientsList {...ingredients}/>*/}
+      </ul>
+      <footer className={style.footer + ' pt-10'}>
+        <FormattedDate date={date}/>
+        <PriceWithCurrency value={total}/>
+      </footer>
+    </article>
+
+  )
+}, ({ id: old }, { id: nxt }) => old === nxt)
