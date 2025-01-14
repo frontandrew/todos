ea9cdf5ac1befafb08ce96dd512a6cbee89f9ee8
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-14 11:21:02 +0500

    chore: move ingredients calculation into deeper component

--- src/features/orders/view/orders-list/view.tsx
+++ src/features/orders/view/orders-list/view.tsx
@@ -3,8 +3,6 @@ import { Link, useLocation } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from 'hooks'
 
 import { OrderItem } from 'entities/order'
-import { Ingredient } from 'entities/ingredient'
-import { ingredientsSlice } from 'features/burger-ingredients' //TODO: sin
 
 import { OrdersAffiliation, ordersSlice } from '../../model'
 import style from './style.module.css'
@@ -14,8 +12,6 @@ export const OrdersList: FC<{ affiliation: OrdersAffiliation }> = ({ affiliation
   const dispatch = useAppDispatch()
 
   let { orders: sortedOrders } = useAppSelector(ordersSlice.selectors.state)
-  const ingrs = useAppSelector(ingredientsSlice.selectors.getState)
-    .reduce((acc, ingr) => acc.set(ingr.id, ingr), new Map<string, Ingredient>())
 
   if (affiliation === 'user' && sortedOrders.length > 0) {
     sortedOrders = [...sortedOrders].reverse()
@@ -29,32 +25,21 @@ export const OrdersList: FC<{ affiliation: OrdersAffiliation }> = ({ affiliation
   }, [])
 
   return (
-      <ul className={style.container}>
-        {sortedOrders.length <= 0 ? null : sortedOrders
-          .map(({ ingredients, ...rest }) => {
-            const orderIngredients = ingredients
-              .reduce((acc, ingrId) => {
-                const ingr = ingrs.get(ingrId)
-                if (!ingr) return acc
-                return [...acc, ingr]
-              }, [] as Array<Ingredient>)
-
-            return (
-              <li key={rest.id}>
-                <Link
-                  to={`${location.pathname}/${rest.number}`}
-                  state={{ backgroundLocation: location }}
-                  style={{ color: 'inherit' }}
-                >
-                  <OrderItem
-                    ingredients={orderIngredients}
-                    {...rest}
-                  />
-                </Link>
-              </li>
-            )
-          })
-        }
-      </ul>
+    <ul className={style.container}>
+      {sortedOrders.length <= 0 ? null : sortedOrders
+        .map((order) => (
+            <li key={order.id}>
+              <Link
+                to={`${location.pathname}/${order.number}`}
+                state={{ backgroundLocation: location }}
+                style={{ color: 'inherit' }}
+              >
+                <OrderItem {...order}/>
+              </Link>
+            </li>
+          )
+        )
+      }
+    </ul>
   )
 }
