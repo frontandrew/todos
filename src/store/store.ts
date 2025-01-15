beaf2ef35f91a3d452d3acf339fc335968976d91
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-15 08:49:06 +0500

    feat: use api/ws

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import { appLoaderSlice } from 'features/app-loader'
-import { apiSlice } from 'api'
+import { apiSlice, wsMiddleware } from 'api'
 
 import { rootReducer } from './root-reducer'
-import { ordersSlice } from 'features/orders'
 
 export const setupStore = () => {
   return configureStore({
     devTools: process.env.NODE_ENV !== 'production',
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-      .concat(apiSlice.middleware, appLoaderSlice.middleware, ordersSlice.middleware),
+      .concat(apiSlice.middleware, appLoaderSlice.middleware, wsMiddleware),
   })
 }
